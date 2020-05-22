693e05785cd7cea2a276bb18d1e1bbb468fd36bb
Fernando Navarro <kevin.navarrogrz@gmail.com>
2020-05-22 00:25:55 -0500

    Agregadas vistas y plantilla para ver los detalles del post en el blog en una página distinta, y CSS para las vistas.

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.utils import timezone #Para poder ordenar los datos por fecha
+from .models import Post #Para poder usar los objetos post de la db
 
-# Create your views here.
+
 def post_list(request, *args, **kwargs):
-    return render(request, 'blog/post_list.html', {})
+    #La línea siguiente filtra los objetos Post recibidos por fecha de publicación
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+def post_detail(request, pk):  #Pk es la clave primaria, la base de datos asigna una a cada post
+    post = get_object_or_404(Post, pk=pk) #Si no existe el objeto regresa error 404
+    return render(request, 'blog/post_detail.html', {'post': post})
