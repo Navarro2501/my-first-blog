53b44b69d50635a169f9d92681eadc7234564fc7
Fernando Navarro <kevin.navarrogrz@gmail.com>
2020-05-22 01:40:19 -0500

    Añadido de vistas para crear y editar posts dentro del sitio.

---
+++ blog/forms.py
@@ -0,0 +1,10 @@
+#Ayuda a crear los formularios para realizar los posts.
+from django import forms
+
+from .models import Post
+
+class PostForm(forms.ModelForm):
+
+    class Meta:
+        model = Post
+        fields = ('title', 'text')
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone #Para poder ordenar los datos por fecha
 from .models import Post #Para poder usar los objetos post de la db
+from .forms import PostForm
 
 
 def post_list(request, *args, **kwargs):
@@ -12,3 +13,33 @@ def post_list(request, *args, **kwargs):
 def post_detail(request, pk):  #Pk es la clave primaria, la base de datos asigna una a cada post
     post = get_object_or_404(Post, pk=pk) #Si no existe el objeto regresa error 404
     return render(request, 'blog/post_detail.html', {'post': post})
+
+
+def post_new(request):
+    #Si al momento de ir al servidor/post/new/ mandamos una petición del tipo post:
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
