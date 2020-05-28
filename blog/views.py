4ee010ef5c6715bd3196cfb2e9eec03216f41722
Fernando Navarro <kevin.navarrogrz@gmail.com>
2020-05-28 16:26:23 -0500

    Agregada la función de crear borradores para publicarlos después; ahora sólo puedes editar tus propios posts; puedes eliminar posts con el botón eliminar (x).

--- blog/views.py
+++ blog/views.py
@@ -24,7 +24,6 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
@@ -60,3 +59,20 @@ def sign_up(request):
     else:
         form = UserCreationForm()
     return render(request, 'blog/sign_up.html', {'form': form})
+
+
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
+
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=pk)
+
+
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('post_list')
